cdb5c198dfdb70d85ecc2a4e9f7cfb22f2cb93fd
lutfillahmafazi <luthfimafazi18@gmail.com>
2019-04-15 21:52:11 +0700

    Initial commit

---
+++ app/src/main/java/co/luthfillahmafazi/multylanguage/MainActivity.java
@@ -0,0 +1,37 @@
+package co.luthfillahmafazi.multylanguage;
+
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txtView = findViewById(R.id.txtView);
+        txtView.setText(getResources().getString(R.string.hello_word));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_change_setting) {
+            Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
